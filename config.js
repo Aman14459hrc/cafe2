ad4cf79964daa86c54542a94b4f9b1a29d4b1b48
Aman mathur <146582108+Aman14459hrc@users.noreply.github.com>
2026-04-14 13:52:07 +0530

    Add global configuration for cafe

---
+++ config.js
@@ -0,0 +1,28 @@
+const config = {
+  cafeName: 'Cafe Delight',
+  colors: {
+    primary: '#FF5733',
+    secondary: '#C70039',
+    background: '#FFC300'
+  },
+  businessHours: {
+    Monday: '08:00 - 20:00',
+    Tuesday: '08:00 - 20:00',
+    Wednesday: '08:00 - 20:00',
+    Thursday: '08:00 - 20:00',
+    Friday: '08:00 - 22:00',
+    Saturday: '09:00 - 22:00',
+    Sunday: '09:00 - 18:00'
+  },
+  contact: {
+    email: 'contact@cafedelight.com',
+    phone: '+1-234-567-8900'
+  },
+  socialMedia: {
+    facebook: 'https://facebook.com/cafedelight',
+    twitter: 'https://twitter.com/cafedelight',
+    instagram: 'https://instagram.com/cafedelight'
+  }
+};
+
+export default config;
